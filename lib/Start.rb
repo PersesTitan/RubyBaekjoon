7ff0c1f34834b087ba2884318cdb60a38affff09
PersesTitan <torampersestitan@gmail.com>
2022-04-29 00:23:24 +0900

    commit

--- lib/Start.rb
+++ lib/Start.rb
@@ -3,4 +3,11 @@ require "Start/version"
 module Start
   class Error < StandardError; end
   # Your code goes here...
+  # @hello = "Hello world"
+  # 변수 사용하기
+
+  a = 1
+  b = "ruby"
+  String text = 10
+  c,d = 3, 4
 end
